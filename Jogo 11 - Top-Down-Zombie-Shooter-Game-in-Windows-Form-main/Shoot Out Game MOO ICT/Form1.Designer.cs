184fce5ebee999f12c8912bf13df74965044351e
Pedro Cravo <125738694+pedcravo@users.noreply.github.com>
2024-09-19 08:46:16 -0300

    Add files via upload

---
+++ Jogo 11 - Top-Down-Zombie-Shooter-Game-in-Windows-Form-main/Shoot Out Game MOO ICT/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Shoot_Out_Game_MOO_ICT
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            txtAmmo = new Label();
+            txtScore = new Label();
+            txtVertical = new Label();
+            txtHorizontal = new Label();
+            label1 = new Label();
+            healthBar = new ProgressBar();
+            player = new PictureBox();
+            GameTimer = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)player).BeginInit();
+            SuspendLayout();
+            // 
+            // txtAmmo
+            // 
+            txtAmmo.AutoSize = true;
+            txtAmmo.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtAmmo.ForeColor = Color.White;
+            txtAmmo.Location = new Point(15, 15);
+            txtAmmo.Margin = new Padding(4, 0, 4, 0);
+            txtAmmo.Name = "txtAmmo";
+            txtAmmo.Size = new Size(93, 24);
+            txtAmmo.TabIndex = 0;
+            txtAmmo.Text = "Ammo: 0";
+            // 
+            // txtScore
+            // 
+            txtScore.AutoSize = true;
+            txtScore.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtScore.ForeColor = Color.White;
+            txtScore.Location = new Point(429, 15);
+            txtScore.Margin = new Padding(4, 0, 4, 0);
+            txtScore.Name = "txtScore";
+            txtScore.Size = new Size(71, 24);
+            txtScore.TabIndex = 0;
+            txtScore.Text = "Kills: 0";
+            // 
+            // txtVertical
+            // 
+            txtVertical.AutoSize = true;
+            txtVertical.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtVertical.ForeColor = Color.White;
+            txtVertical.Location = new Point(450, 10);
+            txtVertical.Margin = new Padding(4, 0, 4, 0);
+            txtVertical.Name = "txtVertical";
+            txtVertical.Size = new Size(71, 24);
+            txtVertical.TabIndex = 0;
+            txtVertical.Text = "Vertical: 0";
+            // 
+            // txtHorizontal
+            // 
+            txtHorizontal.AutoSize = true;
+            txtHorizontal.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtHorizontal.ForeColor = Color.White;
+            txtHorizontal.Location = new Point(455, 10);
+            txtHorizontal.Margin = new Padding(4, 0, 4, 0);
+            txtHorizontal.Name = "txtHorizontal";
+            txtHorizontal.Size = new Size(71, 24);
+            txtHorizontal.TabIndex = 0;
+            txtHorizontal.Text = "Horizontal: 0";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(750, 15);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 24);
+            label1.TabIndex = 0;
+            label1.Text = "Health: ";
+            // 
+            // healthBar
+            // 
+            healthBar.Location = new Point(846, 15);
+            healthBar.Margin = new Padding(4, 3, 4, 3);
+            healthBar.Name = "healthBar";
+            healthBar.Size = new Size(218, 27);
+            healthBar.TabIndex = 1;
+            healthBar.Value = 100;
+            // 
+            // player
+            // 
+            player.Image = Properties.Resources.up;
+            player.Location = new Point(528, 568);
+            player.Margin = new Padding(4, 3, 4, 3);
+            player.Name = "player";
+            player.Size = new Size(71, 100);
+            player.SizeMode = PictureBoxSizeMode.AutoSize;
+            player.TabIndex = 2;
+            player.TabStop = false;
+            player.Click += player_Click;
+            // 
+            // GameTimer
+            // 
+            GameTimer.Enabled = true;
+            GameTimer.Interval = 20;
+            GameTimer.Tick += MainTimerEvent;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 64, 64);
+            ClientSize = new Size(1078, 763);
+            Controls.Add(player);
+            Controls.Add(healthBar);
+            Controls.Add(label1);
+            Controls.Add(txtScore);
+            Controls.Add(txtAmmo);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Zombie Shootout Game MOO ICT";
+            Load += Form1_Load;
+            KeyDown += KeyIsDown;
+            KeyUp += KeyIsUp;
+            ((System.ComponentModel.ISupportInitialize)player).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtAmmo;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label txtVertical;
+        private System.Windows.Forms.Label txtHorizontal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar healthBar;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
